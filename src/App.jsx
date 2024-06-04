8278e463cea43851990c22b5417237752cb78285
Mandy Chen <mandychen.art@gmail.com>
2024-06-04 02:22:31 -0700

    titles

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Notation Map</h1>
+    <h2>Note:&nbsp;
+      <span
+      contentEditable
+      style={{ display:'inline-block', minWidth:'2em'}}
+      
+    >
+
+    </span>
+    </h2>
+    
+    </>
+  );
+}
+
+export default App;
