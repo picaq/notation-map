0193d83b9e8aa11c1bdce591f2988bda9db95bd5
Mandy Chen <mandychen.art@gmail.com>
2024-06-04 02:36:46 -0700

    install react-vexflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import { Score } from 'react-vexflow';
+
 function App() {
   return (
     <>
@@ -15,6 +17,16 @@ function App() {
     </span>
     </h2>
     
+    <Score
+    staves={[
+      ['g3', 'd4', 'e4', 'd4'],
+      ['a4', 'd4', 'e4', 'd4'],
+      ['a4', 'a4', 'b4', 'a4'],
+      ['d4', 'e4', ['g3', 2]],
+    ]}
+    />
+
+
     </>
   );
 }
