bd146dbaa8dcfe1c0cbd9e586ba19f79a3e51688
Mandy Chen <mandychen.art@gmail.com>
2024-06-04 23:49:44 -0700

    add arrow key input navigation but sometimes breaks input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Vex, Stave, StaveNote, Formatter, Flow, Factory, EasyScore} from "vexflow";
 
@@ -18,6 +18,8 @@ for (let i = 0; i <= 9 ; i++) {
   flats.push(...letters.map( a => a + 'b' + i ));
 }
 
+const matrix = [flats, diatonic, sharps];
+
 function App() {
   const [note, setNote] = useState('C4');
   const [inputNote, setInputNote] = useState('C4');
@@ -25,18 +27,40 @@ function App() {
   const [index, setIndex] = useState(28);
   const [accidental, setAccidental] = useState(0);
 
+  useEffect(() => {
+    setNote(matrix[accidental+1][index]);
+    setDisplayNote((matrix[accidental+1][index]?.replace(/(?<=[a-gA-G])b(?=\d{1})/, '♭')?.replace('#', '♯')));
+  }, [accidental, index]);
+
+
+  useEffect(() => {
+    setDisplayNote(note?.replace(/(?<=[a-gA-G])b(?=\d{1})/, '♭')?.replace('#', '♯'));
+    const factory = new Factory({
+      renderer: { elementId: "output", width: 175, height: 255 },
+    });
+    
+    const score = factory.EasyScore();
+    factory
+        .System()
+        .addStave({
+            voices: [
+              score.voice(score.notes(`C#5/q, ${!!note ? note : 'C4'}/q, C#5/h`)), 
+            ],
+        })
+        .addClef("treble")
+    factory.draw();
+  }, [note]);
+
   const clear = () => {
     document.querySelector('#output').innerText = '';
+    document.querySelectorAll('#output svg')?.forEach(s => s.remove());
   };
-  const factory = new Factory({
-    renderer: { elementId: "output", width: 175, height: 255 },
-  });
-  
+
   const updateSvg = (n) => {
-    clear();
-    if (!!n.match(/^[A-Ga-g]{1}[b,#]{0,1}\d{1}$/)) { 
+    if (!!n?.match(/^[A-Ga-g]{1}[b,#]{0,1}\d{1}$/)) { 
+      clear();
       setNote(n);
-      if (!!n.match('b')) {
+      if (!!n?.match(/([a-gA-G])b(\d{1})/)) {
         setIndex(flats.indexOf(n));
         setAccidental(-1);
       } else if (!!n.match('#')) {
@@ -46,24 +70,46 @@ function App() {
         setIndex(diatonic.indexOf(n));
         setAccidental(0);
       }
-      setIndex(diatonic.indexOf(n))
-      setDisplayNote((n.replace(/(?<=[a-gA-G])b(?=\d{1})/, '♭').replace('#', '♯')));
     }
-    document.querySelector('#output svg')?.remove();
   }
+  
+  const changeNote = (key) => {
+    if (!!key.match(/^Arrow/)) {
+      if (key === "ArrowUp") {
+        if (index < 69) { 
+          clear();
+          setIndex(index + 1); 
+        }
+      } else if (key === "ArrowDown") {
+        if (index > 0) { 
+          clear();
+          setIndex(index - 1); 
+        }
+      } else if (key === "ArrowLeft") {
+        if (accidental > -1) { 
+          clear();
+          setAccidental(accidental - 1); 
+        }
+      } else if (key === "ArrowRight") {
+        if (accidental < 1) { 
+          clear();
+          setAccidental(accidental + 1); 
+        }
+      } else {
+        updateSvg(inputNote);
+      }
+      setNote(matrix[accidental+1][index]);
+      // updateSvg(matrix[accidental+1][index]);
+      console.log({accidental});
+      console.log({index});
+      console.log({note});
+    }
+  }
+  useEffect(() => {
+    updateSvg(inputNote);
+  }, [inputNote]);
 
-  const score = factory.EasyScore();
-  factory
-      .System()
-      .addStave({
-          voices: [
-            score.voice(score.notes(`C#5/q, ${note}/q, C#5/h`)), 
-          ],
-      })
-      .addClef("treble")
-  factory.draw();
-
-  document.querySelector('#output .vf-stavenote:nth-of-type(even)').draggable = true;
+  // document.querySelector('#output .vf-stavenote:nth-of-type(even)').draggable = true;
 
   return (
     <>
@@ -72,16 +118,22 @@ function App() {
     <h2>Current note: <span> {displayNote} </span></h2>
     <label>Note:&nbsp;
       <input
-      style={{ display:'inline-block', minWidth:'2em'}} 
+      pattern="^[A-Ga-g]{1}[b,#]{0,1}\d{1}$"
+      maxLength={3}
+      style={{ display:'inline-block', maxWidth:'5em', textAlign:'center'}} 
       onChange={(e) => {
         setInputNote(e.target.value);
-        updateSvg(e.target.value);
       }}
+      onKeyDown={ e => changeNote(e.key) }
+      onKeyUp={ e => !note && updateSvg(e.target.value) }
       value={inputNote}
     >
       
     </input>
     </label>
+    <button type="submit"
+      onClick={e => {e.preventDefault()} }
+    >submit</button>
     
     </>
   );
